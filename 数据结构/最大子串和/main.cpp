8c897b608bddf3334b88714314119cbe403b26aa
mayingshi <yingshi.ma@outlook.com>
2016-03-13 15:44:09 +0800

    modify

--- 数据结构/最大子串和/main.cpp
+++ 数据结构/最大子串和/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <stdio.h>
 #define MAXSIZE 100
 using namespace std;
 int MaxSum(int [],int);
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a[10];
+    int i=0;
+    while(i < 10){
+        scanf("%d ",&a[i]);
+        i++;
+    }
+    printf("%d\n",MaxSum(a,10));
     return 0;
 }
 int MaxSum(int a[],int n)
